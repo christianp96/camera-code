5defa8305b4b70425a89a9c9df475090203e7fab
kds123 <>
2019-11-08 12:18:01 +0200

    roi image

--- code/test.py
+++ code/test.py
@@ -1,12 +1,10 @@
-from src.load_model import LoadedModel
-from src.preprocess import preprocess_greyscale
+import os
 import cv2
 import numpy as np
+import subprocess
 
-cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink", cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink" , cv2.CAP_GSTREAMER)
 
-sessionSavePath = "./session"
-model=LoadedModel(sessionSavePath)
 dim=(32,32)
 
 if cap.isOpened():
@@ -15,14 +13,13 @@ if cap.isOpened():
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) == ord('q'):
       cv2.imwrite('image.jpg',frame)
-      roi=frame[15:520,265:745]
+      roi=frame[15:520,265:770]
+      roi=cv2.resize(roi,dim,cv2.INTER_AREA)
+      for i in range(3):
+        roi[:,:,i] = cv2.equalizeHist(roi[:,:,i]) # histogram equalisation
       cv2.imwrite('roi.jpg',roi)
-      c=cv2.resize(c,dim,cv2.INTER_AREA)
-      #cv2.imwrite('image.jpg',c)
-      c=[cv2.cvtColor(c,cv2.COLOR_BGR2RGB)]
-      c=np.asarray(c,dtype=np.uint8)
-      prediction = model.predict(preprocess_greyscale(c))
-      print(np.argmax(prediction))
+      prediction = os.popen('curl -X POST 0.0.0.0:81/predict -F "imagefile=@roi.jpg"').read()
+      print("Predcited label: ", prediction)
       break
 else:
   print('camera failed to open')
